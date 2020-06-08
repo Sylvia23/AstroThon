298b7bf4cf5f64116c307e3453e1ea8966209de6
Manav Mehta <tmanavmehta@gmail.com>
2020-06-08 23:39:18 +0530

    Revert "Q6: iter3"
    This reverts commit 5b8d56d1cd628479a222c8fa6ff5892128665acb.

--- q6/main.py
+++ q6/main.py
@@ -1,9 +1,6 @@
 # all imports below
 import math
 
-def sin(x): return math.sin(math.radians(x))
-def cos(x): return math.cos(math.radians(x))
-
 g = 9.81
 h =  8848
 re = 6371000
@@ -13,15 +10,18 @@ lon1 = 86.9250
 def findstrike(velocity: float, alt: float, az: float):
    	
 	u = velocity
-	r1 = ((u**2)*(sin(2*az)))/g
-	r2 = ((u*cos(az))/g)
-	r2_= math.sqrt(((u*sin(az))**2) + (2*g*h)) - (u*sin(az))
+	alt = math.radians(alt)
+	az = math.radians(az)
+	r1 = ((u**2)*(math.sin(2*az)))/g
+	r2 = ((u*math.cos(az))/g)
+	r2_= math.sqrt(((u*math.sin(az))**2) + (2*g*h)) - (u*math.sin(az))
 	r2 = r2*r2_
 	r = r1+r2
+	print(r)
 	b = r/re
 
-	a = math.degrees(math.acos(	cos(b)*cos(90-lat1) + (sin(90-lat1)*sin(b)*cos(az))	))
-	B = math.degrees(math.asin(	(sin(b)*sin(az))/sin(a)	))
+	a = math.acos(	math.cos(b)*math.cos(90-lat1) + (math.sin(90-lat1)*math.sin(b)*math.cos(az))	)
+	B = math.asin(	(math.sin(b)*math.sin(az))/math.sin(a)	)
 	lat2 = round(90-a,5)
 	lon2 = round(lon1+B,5)
 
