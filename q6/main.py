17863a395b233fcfdda5d1714afa04516244e903
Manav Mehta <tmanavmehta@gmail.com>
2020-06-08 23:06:57 +0530

    Q6: iter1

--- q6/main.py
+++ q6/main.py
@@ -1,20 +1,32 @@
 # all imports below
+import math
 
-"""
-Any extra lines of code (if required)
-as helper for this function.
-"""
+g = 9.81
+h =  8848
+re = 6371000
+lat1 = 27.9881
+lon1 = 86.9250
 
-def findstrike(velocity, alt, az):
-	'''
-	Parameters
-	----------
-	velocity : A `float`
-    alt: A `float`
-    az: A `float`
-	
-	Returns
-	-------
-	A `tuple` of two floats
-	'''
-	return NotImplementedError
+def findstrike(velocity: float, alt: float, az: float):
+   	
+	u = velocity
+	alt = math.radians(alt)
+	az = math.radians(az)
+	r1 = ((u**2)*(math.sin(2*az)))/g
+	r2 = ((u*math.cos(az))/g)
+	r2_= math.sqrt(((u*math.sin(az))**2) + (2*g*h)) - (u*math.sin(az))
+	r2 = r2*r2_
+	r = r1+r2
+	print(r)
+	b = r/re
+
+	a = math.acos(	math.cos(b)*math.cos(90-lat1) + (math.sin(90-lat1)*math.sin(b)*math.cos(az))	)
+	B = math.asin(	(math.sin(b)*math.sin(az))/math.sin(a)	)
+	lat2 = 90-a
+	lon2 = lon1+B
+
+	return tuple([lat2,lon2])
+
+print(findstrike(float(input()), float(input()), float(input())))
+
+# print(findstrike(10,40,0))
